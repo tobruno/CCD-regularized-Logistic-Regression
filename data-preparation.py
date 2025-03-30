9b7be8ea0cebd7edb16c17b52663e27f83267d2e
tobruno <bruno.tobiasz.13@gmail.com>
2025-03-30 14:02:50 +0200

    Update for preprocessing step

--- data-preparation.py
+++ data-preparation.py
@@ -3,45 +3,73 @@ import numpy as np
 from sklearn.datasets import load_breast_cancer, load_wine, make_classification
 import seaborn as sns
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from ucimlrepo import fetch_ucirepo 
+from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 def data_preprocessing(df):
-    target = df.target
-    df = df.drop(columns=["target"])
-    numerical_columns = df.select_dtypes(include=[np.number]).columns
-    imputer = SimpleImputer(strategy='mean')
-    scaler = StandardScaler()
-    df[numerical_columns] = imputer.fit_transform(df[numerical_columns])
-    df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
+
+    X = df.drop(columns=["target"])
+    y = df.target
+    print(X.info())
+
+    num_features = X.select_dtypes(include=["int64", "float64"]).columns.tolist()
+    cat_features = X.select_dtypes(include=["object", "category"]).columns.tolist()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    num_pipeline = Pipeline([
+        ("imputer", SimpleImputer(strategy="mean")),
+        ("scaler", StandardScaler())
+    ])
+
+    if len(cat_features) != 0:
+        cat_pipeline = Pipeline([
+            ("imputer", SimpleImputer(strategy="most_frequent")),
+            ("encoder", OneHotEncoder(handle_unknown="ignore"))
+        ])
+
+        preprocessor = ColumnTransformer([
+            ("num", num_pipeline, num_features),
+            ("cat", cat_pipeline, cat_features)
+        ])
+    else: 
+        preprocessor = ColumnTransformer([
+            ("num", num_pipeline, num_features)
+        ])
 
 
-    categorical_columns = df.select_dtypes(include=[object, 'category']).columns
-    if len(categorical_columns) != 0:
-        imputer = SimpleImputer(strategy='most_frequent')
-        df[categorical_columns] = imputer.fit_transform(df[categorical_columns])
-        df = pd.get_dummies(df, columns=categorical_columns, drop_first=True)
-    df['target'] = target
+    # Fit & transform training data, transform test data
+    X_train_processed = preprocessor.fit_transform(X_train)
+    X_test_processed = preprocessor.transform(X_test)
 
-    return df
+    # Convert back to DataFrame
+    X_train_final = pd.DataFrame(X_train_processed, columns=preprocessor.get_feature_names_out())
+    X_test_final = pd.DataFrame(X_test_processed, columns=preprocessor.get_feature_names_out())
+
+    return X_train_final, X_test_final, y_train, y_test
+
+
 
 def get_cancer_data():
     data_cancer = load_breast_cancer()
     breast_cancer = pd.DataFrame(data_cancer.data, columns=data_cancer.feature_names)
     breast_cancer['target'] = data_cancer.target
 
-    breast_cancer = data_preprocessing(breast_cancer)   
-    return breast_cancer
+    X_train, X_test, y_train, y_test = data_preprocessing(breast_cancer)
+    return X_train, X_test, y_train, y_test
 
 def get_wine_data():
     data_wine = load_wine()
     wine = pd.DataFrame(data_wine.data, columns=data_wine.feature_names)
     wine['target'] = data_wine.target
 
-    wine = data_preprocessing(wine)
-    return wine
+    X_train, X_test, y_train, y_test = data_preprocessing(wine)
+    return X_train, X_test, y_train, y_test
 
 def get_heart_data():
     data = fetch_ucirepo(id=45) 
@@ -49,18 +77,17 @@ def get_heart_data():
     target = data.data.targets 
     heart_data['target'] = target
 
-    print(heart_data.target.value_counts())
 
-    heart_data = data_preprocessing(heart_data)   
-    return heart_data
+    X_train, X_test, y_train, y_test = data_preprocessing(heart_data)
+    return X_train, X_test, y_train, y_test
 
 def get_titatnic_data():
     df = sns.load_dataset("titanic")
     titanic = df.rename(columns={'survived':'target'})
-    titanic = titanic.drop(columns=["deck"])
+    titanic = titanic.drop(columns=["deck", "fare"])
 
-    titanic = data_preprocessing(titanic)
-    return titanic
+    X_train, X_test, y_train, y_test = data_preprocessing(titanic)
+    return X_train, X_test, y_train, y_test
 
 
 def get_synthetic_data():
@@ -69,60 +96,54 @@ def get_synthetic_data():
     column_names = [f"feature_{i}" for i in range(X.shape[1])]
     df = pd.DataFrame(X, columns=column_names)
     df["target"] = y
-    return df
+
+    X_train, X_test, y_train, y_test = data_preprocessing(df)
+    return X_train, X_test, y_train, y_test
 
 
 # QUICK TESTS
 
-# df = get_cancer_data()
-# df.info()
-# X = df.drop(columns = ['target'])
-# y = df.target
+# print('Cancer')
+# X_train, X_test, y_train, y_test = get_cancer_data()
 # lg = LogisticRegression()
-# lg.fit(X, y)
+# lg.fit(X_train, y_train)
 
-# pred = lg.predict(X)
-# print(accuracy_score(y, pred))
+# pred = lg.predict(X_test)
+# print(accuracy_score(y_test, pred))
 
-# df = get_heart_data()
-# df.info()
-# X = df.drop(columns = ['target'])
-# y = df.target
+print('Heart')
+X_train, X_test, y_train, y_test = get_heart_data()
+lg = LogisticRegression()
+lg.fit(X_train, y_train)
+
+pred = lg.predict(X_test)
+print(accuracy_score(y_test, pred))
+
+
+# print('Wine')
+# X_train, X_test, y_train, y_test = get_wine_data()
 # lg = LogisticRegression()
-# lg.fit(X, y)
+# lg.fit(X_train, y_train)
 
-# pred = lg.predict(X)
-# print(accuracy_score(y, pred))
+# pred = lg.predict(X_test)
+# print(accuracy_score(y_test, pred))
 
-# df = get_synthetic_data()
-# df.info()
-# X = df.drop(columns = ['target'])
-# y = df.target
+
+# print('Titanic')
+# X_train, X_test, y_train, y_test = get_titatnic_data()
 # lg = LogisticRegression()
-# lg.fit(X, y)
+# lg.fit(X_train, y_train)
 
-# pred = lg.predict(X)
-# print(accuracy_score(y, pred))
+# pred = lg.predict(X_test)
+# print(accuracy_score(y_test, pred))
 
-# df = get_titatnic_data()
-# df.info()
-# X = df.drop(columns = ['target'])
-# y = df.target
+# print('Synthetic')
+# X_train, X_test, y_train, y_test = get_synthetic_data()
 # lg = LogisticRegression()
-# lg.fit(X, y)
+# lg.fit(X_train, y_train)
 
-# pred = lg.predict(X)
-# print(accuracy_score(y, pred))
-
-# df = get_wine_data()
-# df.info()
-# X = df.drop(columns = ['target'])
-# y = df.target
-# lg = LogisticRegression()
-# lg.fit(X, y)
-
-# pred = lg.predict(X)
-# print(accuracy_score(y, pred))
+# pred = lg.predict(X_test)
+# print(accuracy_score(y_test, pred))
 
 
 
